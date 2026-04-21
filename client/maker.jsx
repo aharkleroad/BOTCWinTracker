993021a68ed7bfeb90aad712d78131c611174d43
Aizlynn Harkleroad <arh4811@rit.edu>
2026-04-20 22:08:40 -0400

    Created my add game form. Got dynamic components updating correctly

--- client/maker.jsx
+++ client/maker.jsx
@@ -3,7 +3,48 @@ const React = require('react');
 const { useState, useEffect } = React;
 const { createRoot } = require('react-dom/client');
 
-const handleDomo = (e, onDomoAdded) => {
+// dynamically create nav bar based on the user's account status
+const NavBar = (props) => {
+    const [accountStatus, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        // need to access account status 
+        const loadAccountStatus = async () => {
+            const data = { premiumStatus: true };
+            // const response = await fetch('/getDomos');
+            // const data = await response.json();
+            setStatus(data.premiumStatus);
+        }
+
+        loadAccountStatus();
+    }, [props.reloadNav]);
+
+    // premium bar
+    if (accountStatus) {
+        return (
+            <nav className="navBar">
+                <a href="/login"><img id="logo" src="/assets/img/face.png" alt="face logo" /></a>
+                <div class="navlink"><a href="/maker">Games</a></div>
+                <div class="navlink"><a href="/maker">Stats</a></div>
+                <div class="navlink"><a href="/maker">Friends</a></div>
+                <div class="navlink"><a href="/settings">Settings</a></div>
+                <div class="navlink"><a href="/logout">Log out</a></div>
+            </nav>
+        );
+    }
+    // basic bar
+    return (
+        <nav className="navBar">
+            <a href="/login"><img id="logo" src="/assets/img/face.png" alt="face logo" /></a>
+            <div class="navlink"><a href="/maker">Games</a></div>
+            <div class="navlink"><a href="/maker">Stats</a></div>
+            <div class="navlink"><a href="/settings">Settings</a></div>
+            <div class="navlink"><a href="/logout">Log out</a></div>
+        </nav>
+    );
+};
+
+const handleGameCreate = (e, onDomoAdded) => {
     e.preventDefault();
     helper.hideError();
 
@@ -19,57 +60,6 @@ const handleDomo = (e, onDomoAdded) => {
     return false;
 };
 
-const DomoForm = (props) => {
-    return (
-        <form id="domoForm" name="domoForm" className="domoForm"
-            action="/maker" method="POST" onSubmit={(e) => handleDomo(e, props.triggerReload)}>
-
-            <label htmlFor="name">Name: </label>
-            <input id="domoName" type="text" name="name" placeholder="Domo Name" />
-            <label htmlFor="age">Age: </label>
-            <input id="domoAge" type="number" min="0" name="age" />
-            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
-        </form>
-    );
-};
-
-const NavBar = (props) => {
-    const [accountStatus, setStatus] = useState(props.status);
-
-    useEffect(() => {
-        const loadAccountStatus = async () => {
-            const data = { premiumStatus: true };
-            // const response = await fetch('/getDomos');
-            // const data = await response.json();
-            setStatus(data.premiumStatus);
-        }
-
-        loadAccountStatus();
-    }, [props.reloadNav]);
-
-    if (accountStatus) {
-        return (
-            <nav className="navBar">
-                <a href="/login"><img id="logo" src="/assets/img/face.png" alt="face logo" /></a>
-                <div class="navlink"><a href="/maker">Games</a></div>
-                <div class="navlink"><a href="/maker">Stats</a></div>
-                <div class="navlink"><a href="/maker">Friends</a></div>
-                <div class="navlink"><a href="/settings">Settings</a></div>
-                <div class="navlink"><a href="/logout">Log out</a></div>
-            </nav>
-        );
-    }
-    return (
-        <nav className="navBar">
-            <a href="/login"><img id="logo" src="/assets/img/face.png" alt="face logo" /></a>
-            <div class="navlink"><a href="/maker">Games</a></div>
-            <div class="navlink"><a href="/maker">Stats</a></div>
-            <div class="navlink"><a href="/settings">Settings</a></div>
-            <div class="navlink"><a href="/logout">Log out</a></div>
-        </nav>
-    );
-};
-
 const DomoList = (props) => {
     const [domos, setDomos] = useState(props.domos);
 
@@ -82,7 +72,7 @@ const DomoList = (props) => {
         };
 
         loadDomosFromServer();
-    }, [props.reloadDomos]);
+    }, [props.reloadGames]);
 
     if (domos.length === 0) {
         return (
@@ -107,19 +97,111 @@ const DomoList = (props) => {
     );
 };
 
+const Role = (props) => {
+    const allChars = {
+        townsfolk: [{name: "Grandmother"}, {name: "Fortune Teller"}, {name: "Monk"}],
+        outsiders: [{name: "Mutant"}, {name: "Recluse"}, {name: "Plague Doctor"}],
+        minions: [{name: "Scarlet Woman"}, {name: "Poisoner"}, {name: "Goblin"}],
+        demons: [{name: "Imp"}, {name: "Fangu"}, {name: "Lleech"}],
+        travellers: [{name: "Harlot"}, {name: "Scapegoat"}],
+    };
+
+    const [chars, setChars] = useState(props.roleType);
+    useEffect(() => {
+        const getCharType = () => {
+            let charType;
+            if (document.getElementById('charType').value) {
+                charType = document.getElementById('charType').value;
+            }
+            else {
+                charType = props.roleType;
+            }
+            setChars(charType);
+        };
+        getCharType();
+    }, [props.reloadCharacters]);
+
+    const charList = allChars[chars].map(char => {
+        return (
+            <option key={char.name} className="char">{char.name}</option>
+        );
+    });
+
+    return (
+        <>
+        <label htmlFor="charType">Character Type: </label>
+            <select id="charType" name="charType" required>
+                <div className="charList">{charList}</div>
+            </select>
+        </>
+    );
+}
+
+const CharacterType = (props) => {
+    return (
+        <>
+            <label htmlFor="charType">Character Type: </label>
+            <select id="charType" name="charType" onInput={props.triggerCharReload} required>
+                <option value="townsfolk" selected>Townsfolk</option>
+                <option value="outsiders">Outsider</option>
+                <option value="minions">Minion</option>
+                <option value="demons">Demon</option>
+                <option value="travellers">Traveller</option>
+            </select>
+        </>
+    );
+};
+
+const GameForm = (props) => {
+    const [reloadCharacters, setReloadCharacters] = useState(false);
+
+    return (
+        <form id="domoForm" name="domoForm" className="domoForm"
+            action="/maker" method="POST" onSubmit={(e) => handleGameCreate(e, props.triggerReload)}>
+
+            <div>
+                <label htmlFor="script">Script: </label>
+                <select id="script" name="script" required>
+                    <option value="trouble" selected>Trouble Brewing</option>
+                    <option value="sects">Sects and Violets</option>
+                    <option value="badMoon">Bad Moon Rising</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+
+            <div>
+                <label htmlFor="alignment">Starting Alignment: </label>
+                <select id="alignment" name="alignment" required>
+                    <option value="good" selected>Good</option>
+                    <option value="evil">Evil</option>
+                </select>
+            </div>
+
+            <CharacterType triggerCharReload={() => {setReloadCharacters(!reloadCharacters);}} />
+
+            <Role roleType='townsfolk' reloadCharacters={reloadCharacters} />
+
+            <label htmlFor="date">Date Played: </label>
+            <input id="date" type="datetime-local" name="date" />
+
+            <input className="makeDomoSubmit" type="submit" value="Add Game" />
+        </form>
+    );
+};
+
 const App = () => {
-    const [reloadDomos, setReloadDomos] = useState(false);
+    const [reloadGames, setReloadGames] = useState(false);
 
     return (
         <>
             <NavBar reloadNav={false} />
 
-            <div>
-                <div id="makeDomo">
-                    <DomoForm triggerReload={() => setReloadDomos(!reloadDomos)} />
+            <div className="mainPageContents">
+                <div id="createGame">
+                    <GameForm triggerReload={() => setReloadGames(!reloadGames)} />
                 </div>
                 <div id="domos">
-                    <DomoList domos={[]} reloadDomos={reloadDomos} />
+                    <DomoList domos={[]} reloadDomos={reloadGames} />
                 </div>
             </div>
         </>
